d3b863f52ec4a8cf7735a9f3c2736a44b24fc798
iamsalahudin <iamsalahudin123@gmail.com>
2025-12-23 11:51:29 +0500

    chore: remove .next from repo and ignore it

--- frontend/.next/dev/static/chunks/src_app_error_19498d60.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_error_5ae81633.js"
-],
-    source: "dynamic"
-});
